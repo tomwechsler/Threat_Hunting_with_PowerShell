a1106a993e7c5276a6d0e59597445c6c3a6622ef
Tom Wechsler [Microsoft Azure & CDM MVP] <56835948+tomwechsler@users.noreply.github.com>
2023-05-19 13:36:49 +0200

    Delete Tactics_Techniques_Procedures(TTPs) directory

--- Tactics_Techniques_Procedures(TTPs)/Check if windows installer service is running.ps1
+++
@@ -1,6 +0,0 @@
-$service = Get-Service -Name "msiserver" -ErrorAction SilentlyContinue
-if ($service -eq $null) {
-    Write-Host "Windows Installer Service is not installed."
-} else {
-    Write-Host "Windows Installer Service is installed."
-}
